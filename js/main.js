a32dd558ea695bfae33d7985de9ba2e1ca44c19f
Bye-Hong <hby033@naver.com>
2021-08-05 12:35:12 +0900

    bye-Hong
    update : mobile menu 버튼 구현

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Mobile Menu bar Effect Code
+function MobileMenuBar() {
+  const menuIcon = document.querySelector(".menu_ico");
+  const mobileMenu = document.querySelector(".mobile_Menu");
+  const overlay = document.querySelector(".overlay");
+
+  function MobileBtn() {
+    if (menuIcon.classList.contains("active")) {
+      menuIcon.classList.remove("active");
+      mobileMenu.classList.remove("active");
+      overlay.classList.remove("active");
+    } else {
+      menuIcon.classList.add("active");
+      mobileMenu.classList.add("active");
+      overlay.classList.add("active");
+    }
+  };
+  menuIcon.addEventListener("click", () => {
+    MobileBtn()
+  });
+  overlay.addEventListener("click", () => {
+    MobileBtn()
+  });
+
+}
+MobileMenuBar();
